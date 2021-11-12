d99ecafe466284a7fb52793e090805c2c95611a2
Burak <75482610+burakcantokses@users.noreply.github.com>
2021-11-12 23:23:44 +0300

    MainUI %75

--- deltaware/deltaware/Form1.Designer.cs
+++ deltaware/deltaware/Form1.Designer.cs
@@ -28,13 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.anasayfaButton = new System.Windows.Forms.Label();
+            this.yonetimButton = new System.Windows.Forms.Label();
+            this.aidatButton = new System.Windows.Forms.Label();
+            this.giderButton = new System.Windows.Forms.Label();
+            this.cikisButton = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // anasayfaButton
+            // 
+            this.anasayfaButton.AutoSize = true;
+            this.anasayfaButton.BackColor = System.Drawing.Color.White;
+            this.anasayfaButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.anasayfaButton.Font = new System.Drawing.Font("Nunito Sans", 21F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.anasayfaButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(133)))), ((int)(((byte)(198)))));
+            this.anasayfaButton.Location = new System.Drawing.Point(137, 27);
+            this.anasayfaButton.Name = "anasayfaButton";
+            this.anasayfaButton.Size = new System.Drawing.Size(138, 40);
+            this.anasayfaButton.TabIndex = 0;
+            this.anasayfaButton.Text = "Anasayfa";
+            this.anasayfaButton.Click += new System.EventHandler(this.panelClick);
+            this.anasayfaButton.MouseEnter += new System.EventHandler(this.panelMouseEnter);
+            this.anasayfaButton.MouseLeave += new System.EventHandler(this.panelMouseLeave);
+            // 
+            // yonetimButton
+            // 
+            this.yonetimButton.AutoSize = true;
+            this.yonetimButton.BackColor = System.Drawing.Color.White;
+            this.yonetimButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.yonetimButton.Font = new System.Drawing.Font("Nunito Sans", 21F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.yonetimButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(128)))), ((int)(((byte)(140)))));
+            this.yonetimButton.Location = new System.Drawing.Point(297, 27);
+            this.yonetimButton.Name = "yonetimButton";
+            this.yonetimButton.Size = new System.Drawing.Size(122, 40);
+            this.yonetimButton.TabIndex = 1;
+            this.yonetimButton.Text = "Yönetim";
+            this.yonetimButton.Click += new System.EventHandler(this.panelClick);
+            this.yonetimButton.MouseEnter += new System.EventHandler(this.panelMouseEnter);
+            this.yonetimButton.MouseLeave += new System.EventHandler(this.panelMouseLeave);
+            // 
+            // aidatButton
+            // 
+            this.aidatButton.AutoSize = true;
+            this.aidatButton.BackColor = System.Drawing.Color.White;
+            this.aidatButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.aidatButton.Font = new System.Drawing.Font("Nunito Sans", 21F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.aidatButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(128)))), ((int)(((byte)(140)))));
+            this.aidatButton.Location = new System.Drawing.Point(456, 27);
+            this.aidatButton.Name = "aidatButton";
+            this.aidatButton.Size = new System.Drawing.Size(123, 40);
+            this.aidatButton.TabIndex = 2;
+            this.aidatButton.Text = "Aidatlar";
+            this.aidatButton.Click += new System.EventHandler(this.panelClick);
+            this.aidatButton.MouseEnter += new System.EventHandler(this.panelMouseEnter);
+            this.aidatButton.MouseLeave += new System.EventHandler(this.panelMouseLeave);
+            // 
+            // giderButton
+            // 
+            this.giderButton.AutoSize = true;
+            this.giderButton.BackColor = System.Drawing.Color.White;
+            this.giderButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.giderButton.Font = new System.Drawing.Font("Nunito Sans", 21F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.giderButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(128)))), ((int)(((byte)(140)))));
+            this.giderButton.Location = new System.Drawing.Point(605, 27);
+            this.giderButton.Name = "giderButton";
+            this.giderButton.Size = new System.Drawing.Size(123, 40);
+            this.giderButton.TabIndex = 3;
+            this.giderButton.Text = "Giderler";
+            this.giderButton.Click += new System.EventHandler(this.panelClick);
+            this.giderButton.MouseEnter += new System.EventHandler(this.panelMouseEnter);
+            this.giderButton.MouseLeave += new System.EventHandler(this.panelMouseLeave);
+            // 
+            // cikisButton
+            // 
+            this.cikisButton.AutoSize = true;
+            this.cikisButton.BackColor = System.Drawing.Color.White;
+            this.cikisButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cikisButton.Font = new System.Drawing.Font("Nunito Sans", 21F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cikisButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(128)))), ((int)(((byte)(140)))));
+            this.cikisButton.Location = new System.Drawing.Point(754, 27);
+            this.cikisButton.Name = "cikisButton";
+            this.cikisButton.Size = new System.Drawing.Size(79, 40);
+            this.cikisButton.TabIndex = 4;
+            this.cikisButton.Text = "Çıkış";
+            this.cikisButton.Click += new System.EventHandler(this.panelClick);
+            this.cikisButton.MouseEnter += new System.EventHandler(this.panelMouseEnter);
+            this.cikisButton.MouseLeave += new System.EventHandler(this.panelMouseLeave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = global::deltaware.Properties.Resources.resizedMain1;
+            this.ClientSize = new System.Drawing.Size(1026, 668);
+            this.Controls.Add(this.cikisButton);
+            this.Controls.Add(this.giderButton);
+            this.Controls.Add(this.aidatButton);
+            this.Controls.Add(this.yonetimButton);
+            this.Controls.Add(this.anasayfaButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "vv";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label anasayfaButton;
+        private System.Windows.Forms.Label yonetimButton;
+        private System.Windows.Forms.Label aidatButton;
+        private System.Windows.Forms.Label giderButton;
+        private System.Windows.Forms.Label cikisButton;
     }
 }
 
--- deltaware/deltaware/Form1.cs
+++ deltaware/deltaware/Form1.cs
@@ -12,9 +12,91 @@ namespace deltaware
 {
     public partial class Form1 : Form
     {
+        public string selectedButtonName = "Anasayfa";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void panelClick(object sender, EventArgs e)
+        {
+            Label label = (Label)sender;
+            string buttonName = label.Text;
+            switch (buttonName)
+            {
+                case "Anasayfa":
+                    anasayfaButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(133)))), ((int)(((byte)(198)))));
+                    unSelectedButton(yonetimButton);
+                    unSelectedButton(aidatButton);
+                    unSelectedButton(giderButton);
+                    break;
+                case "Yönetim":
+                    yonetimButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(133)))), ((int)(((byte)(198)))));
+                    unSelectedButton(anasayfaButton);
+                    unSelectedButton(aidatButton);
+                    unSelectedButton(giderButton);
+                    break;
+                case "Aidatlar":
+                    aidatButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(133)))), ((int)(((byte)(198)))));
+                    unSelectedButton(yonetimButton);
+                    unSelectedButton(anasayfaButton);
+                    unSelectedButton(giderButton);
+                    break;
+                case "Giderler":
+                    giderButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(133)))), ((int)(((byte)(198)))));
+                    unSelectedButton(yonetimButton);
+                    unSelectedButton(anasayfaButton);
+                    unSelectedButton(aidatButton);
+                    break;
+                case "Çıkış":
+                    exit();
+                    break;
+                default:
+                    break;
+            }
+            selectedButtonName = buttonName;
+
+        }
+
+
+
+        private void panelMouseEnter(object sender, EventArgs e)
+        {
+            Label label = (Label)sender;
+            if (selectedButtonName.Equals(label.Text))
+            {
+                label.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(152)))), ((int)(((byte)(187)))));
+            }else
+            {
+                label.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(199)))), ((int)(((byte)(212)))));
+            }
+        }
+
+        private void panelMouseLeave(object sender, EventArgs e)
+        {
+            Label label = (Label)sender;
+            if (selectedButtonName.Equals(label.Text))
+            {
+                label.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(133)))), ((int)(((byte)(198)))));
+            }else
+            {
+                label.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(128)))), ((int)(((byte)(140)))));
+            }
+        }
+
+        public void unSelectedButton(Label label)
+        {
+            label.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(128)))), ((int)(((byte)(140)))));
+        }
+
+        public void exit()
+        {
+            DialogResult dialog = new DialogResult();
+            dialog = MessageBox.Show("Çıkış yapılsın mı?", "Deltaware Projects - Apartman Yönetim Sistemi", MessageBoxButtons.YesNo);
+            if (dialog == DialogResult.Yes)
+            {
+                System.Environment.Exit(0);
+            }
         }
     }
 }
